f86a0477725f5f4f623f15740f2e60e25b7187d6
Rafiq <aazztech6@gmail.com>
2018-12-25 12:32:47 +0600

    change default background image

--- templates/home-page.php
+++ templates/home-page.php
@@ -5,10 +5,8 @@
  * */
 
 get_header('home');
-while (have_posts()) {
-    $home_bg_image = get_theme_mod('directoria_home_bg', get_template_directory_uri().'/images/home_page_bg.jpg');
-    ?>
-    <div class="directoria_home_area_wrapper " style="background-image: url('<?php echo esc_url($home_bg_image); ?>')">
+while (have_posts()) { ?>
+    <div class="directoria_home_area_wrapper ">
         <?php the_post();
         the_content(); ?>
     </div>
