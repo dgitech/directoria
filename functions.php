943c103c6339a0df0e0e4b723813d21d36999987
themeaazz <43126377+themeaazz@users.noreply.github.com>
2019-05-04 11:27:13 +0600

    One click demo importer has been added

--- functions.php
+++ functions.php
@@ -318,29 +318,30 @@ function ocdi_import_files()
             'local_import_file' => trailingslashit(get_template_directory()) . 'ocdi/demo-content.xml',
             'local_import_widget_file' => trailingslashit(get_template_directory()) . 'ocdi/widgets.wie',
             'local_import_customizer_file' => trailingslashit(get_template_directory()) . 'ocdi/customizer.dat',
-            'import_notice' => __('After you import this demo, you will have to setup the slider separately.', 'directoria'),
         ),
     );
 }
 
 add_filter('pt-ocdi/import_files', 'ocdi_import_files');
 
-function directoria_after_import_setup() {
-    // Assign menus to their locations.
-    $primary_menu = get_term_by( 'name', 'Primary Menu', 'nav_menu' );
+function directoria_after_import_setup()
+{
+// Assign menus to their locations.
+    $primary_menu = get_term_by('name', 'Menu 1', 'nav_menu');
 
-    set_theme_mod( 'nav_menu_locations', array(
+    set_theme_mod('nav_menu_locations', array(
             'primary' => $primary_menu->term_id,
         )
     );
 
-    // Assign front page and posts page (blog page).
-    $front_page_id = get_page_by_title( 'Home' );
-    $blog_page_id  = get_page_by_title( 'Blog' );
+// Assign front page and posts page (blog page).
+    $front_page_id = get_page_by_title('Home');
+    $blog_page_id = get_page_by_title('Blog');
 
-    update_option( 'show_on_front', 'page' );
-    update_option( 'page_on_front', $front_page_id->ID );
-    update_option( 'page_for_posts', $blog_page_id->ID );
+    update_option('show_on_front', 'page');
+    update_option('page_on_front', $front_page_id->ID);
+    update_option('page_for_posts', $blog_page_id->ID);
 
 }
-add_action( 'pt-ocdi/after_import', 'directoria_after_import_setup' );
+
+add_action('pt-ocdi/after_import', 'directoria_after_import_setup');
